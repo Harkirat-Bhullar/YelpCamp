fb39e9f0923f09fae6c2c7f199f31b9f9033cffe
Harkirat-Bhullar <harkiratbhullar@live.ca>
2022-02-23 10:40:23 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ const campgroundRoutes = require('./routers/camgrounds');
 const reviewRoutes = require('./routers/reviews');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
@@ -84,10 +88,23 @@ app.use(
     })
 );
 
+const secret = process.env.SECRET || 'thisshouldbeaspecialsecret';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    secret
+});
+
+store.on('error', function(e){
+console.log('Session Store Error',e)
+})
+
 
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'thisshouldbeaspecialsecret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
